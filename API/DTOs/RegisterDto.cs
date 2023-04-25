36737d4a488ebda1b689500d9a8706fa6c6f8ec7
LK205 <khoikaito@gmail.com>
2023-04-25 13:30:00 +0700

    end of section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -11,7 +11,9 @@ namespace API.DTOs
     {
         [Required]
         public string Username { get; set; }
+        
         [Required]
+        [StringLength( 100 ,MinimumLength = 3)]
         public string Password { get; set; }
     }
 }
