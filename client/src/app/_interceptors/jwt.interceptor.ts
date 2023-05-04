5297785dfa894f492a51d1743cbf1b761924b871
LK205 <khoikaito@gmail.com>
2023-05-04 18:16:56 +0700

    end of section 9

---
+++ client/src/app/_interceptors/jwt.interceptor.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, take } from 'rxjs';
+import { AccountService } from '../_services/accout.service';
+import { User } from '../_models/user';
+
+@Injectable()
+export class JwtInterceptor implements HttpInterceptor {
+
+  constructor(private acountService: AccountService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    let currentUser: User;
+    this.acountService.currentUser$.pipe(take(1)).subscribe(user => currentUser = user);
+    if(currentUser! ){
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${currentUser.token}` 
+        }
+      })
+    }
+    return next.handle(request);
+  }
+}
--- client/src/app/_services/accout.service.ts
+++ client/src/app/_services/accout.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
 import { User } from '../_models/user';
 import { ReplaySubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 
 
@@ -10,7 +11,7 @@ import { ReplaySubject } from 'rxjs';
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl = 'https://localhost:5001/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
   
