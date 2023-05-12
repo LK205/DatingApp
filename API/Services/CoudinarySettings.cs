04ebb9b9e7cc1727e3c276c9d47b5df79e505fc0
LK205 <khoikaito@gmail.com>
2023-05-12 16:04:22 +0700

    end of section 11

---
+++ API/Services/CoudinarySettings.cs
@@ -0,0 +1,9 @@
+namespace API.Services
+{
+    public class CoudinarySettings
+    {
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
